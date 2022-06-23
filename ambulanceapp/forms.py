d67963aab99255608b072d272d3ba48951d05d8c
NessaAz <vanessa.azenwa@gmail.com>
2022-06-24 00:36:29 +0300

    update provider form to inherit from addressmixin

--- ambulanceapp/forms.py
+++ ambulanceapp/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from . models import *
 
-class AmbulanceproviderForm(UserCreationForm):
+
+class AddressMixin(forms.ModelForm):
+    class Meta:
+        model = Ambulanceprovider
+        fields = ('address_one', 'address_two', 'city', 'state',)
+        widgets = {'address_one': forms.TextInput(attrs={'class':'form-control'}),
+            'address_two': forms.TextInput(attrs={'class':'form-control'}),
+            'city': forms.TextInput(attrs={'class':'form-control'}),
+            'state': forms.TextInput(attrs={'class':'form-control'}),
+        }    
+
+
+class AmbulanceproviderForm(AddressMixin, UserCreationForm):
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     password1 = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'type':'password'}))
