6444cfdfc24bd7e165f97cf594143f42fc786ea2
NessaAz <vanessa.azenwa@gmail.com>
2022-06-24 00:25:36 +0300

    create the provider model class

--- ambulanceapp/models.py
+++ ambulanceapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Ambulanceprovider(models.Model):
+    user_rec = models.ForeignKey(User, on_delete=models.CASCADE)
+    address_one = models.CharField(max_length=100)
+    address_two = models.CharField(max_length=100, blank=True)
+    county = models.CharField(max_length=50)
+    town = models.CharField(max_length=2)
+    stripe_id = models.CharField(max_length=30, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Ambulanceproviders'
+
+    def __unicode__(self):
+        return u"%s's Provider Info" % self.user_rec
