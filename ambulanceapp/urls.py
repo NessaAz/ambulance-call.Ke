b2e864d64856afeadba7d5b7aacbcc0338f8c20a
NessaAz <vanessa.azenwa@gmail.com>
2022-06-24 03:57:07 +0300

    correct url path of accounts list

--- ambulanceapp/urls.py
+++ ambulanceapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('signup/', views.provider, name='provider' ),
     path('login/', views.LoginView.as_view(template_name='ambulanceapp/login.html'), name='login'),
     path('logout/',views.LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),        
-    path('account/list', views.AccountList.as_view(), name='account_list'),
+    path('account/list/', views.AccountList.as_view(), name='account_list'),
     
 ]
